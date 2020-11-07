69de2ac6eb9cbcf8012eb111398f528059c523cb
dadop44 <58806599+dadop44@users.noreply.github.com>
2020-11-07 19:41:04 +0700

    add class FirstPage

--- lib/main.dart
+++ lib/main.dart
@@ -17,3 +17,36 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routing App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'First Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            RaisedButton(
+              child: Text('Go to second'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        SecondPage(data: 'Hello! Wellcome to the Second Page'),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
