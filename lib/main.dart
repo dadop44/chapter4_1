932dbfe325ba47762efa9c0023a18ff0b9f0e233
dadop44 <58806599+dadop44@users.noreply.github.com>
2020-11-07 19:49:47 +0700

    add class SecondPage

--- lib/main.dart
+++ lib/main.dart
@@ -50,3 +50,36 @@ class FirstPage extends StatelessWidget {
     );
   }
 }
+
+class SecondPage extends StatelessWidget {
+  final String data;
+
+  SecondPage({
+    Key key,
+    @required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Routing App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'Second Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            Text(
+              data,
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
